97e9e466c73273bf4936830d4aed53d1b85f578e
char543 <36009210+char543@users.noreply.github.com>
2022-07-22 02:05:09 +0100

    updates to id's page

--- src/components/Identities.jsx
+++ src/components/Identities.jsx
@@ -1,24 +1,80 @@
-import React from 'react'
+import React from "react";
+import { Card, Button } from "react-daisyui";
 
 function Identities() {
   return (
     <>
       <div className="flex justify-start items-center h-[100%] pt-24 flex-col overflow-scroll sm:justify-center">
-        <p className="z-20 text-5xl text-white font-bold m-0 p-0 mb-8">Identities</p>
-        <div id='idCol' className='flex flex-col justify-center box-content items-center max-w-[200px] mb-12 sm:flex-row'>
-          <img className='rounded-box object-fit w-full m-4 h-[200px]' src="nightsjourney.jpg" alt="nightsjourney" />
-          <img className='rounded-box object-fit w-full m-4 h-[200px]' src="room.jpg" alt="room" />
-          <img className='rounded-box object-cover w-full m-4 h-[200px]' src="circuit.jpg" alt="room" />
+        <p className="z-20 text-5xl text-white font-bold m-0 p-0 mb-12">
+          Identities
+        </p>
 
-          <div className='cardsContainer flex'>
-            <div className='card flex'>
-              <img src="circuit.png" alt="circuit-png" />
-              <a href="/">circuit.global</a>
-            </div>
-          </div>
+        {/*  */}
+
+        <div className="cardContainer flex justify-start items-center flex-col flex-wrap mb-16 box-content sm:flex-row sm:justify-center sm:w-full">
+          <Card className="w-[256px] m-4 min-w-[256px]">
+            <Card.Image
+              className="px-14 py-20 h-[180px]"
+              src="circuit.png"
+              alt="Circuit"
+            />
+            <Card.Body>
+              <Card.Title tag="h2">Room Life!</Card.Title>
+              <p>Check out the amazing Room Life brand now!</p>
+              <Card.Actions className="justify-end py-2">
+                <Button color="primary">View Now</Button>
+              </Card.Actions>
+            </Card.Body>
+          </Card>
+
+          <Card className="w-[256px] m-4 min-w-[256px]">
+            <Card.Image
+              className="object-fill w-full h-[180px]"
+              src="nightsjourney.jpg"
+              alt="nightsjourney"
+            />
+            <Card.Body>
+              <Card.Title tag="h2">Nights Journey!</Card.Title>
+              <p>Check out the amazing Nights Journey now!</p>
+              <Card.Actions className="justify-end py-2">
+                <Button color="primary">View Now</Button>
+              </Card.Actions>
+            </Card.Body>
+          </Card>
+
+          <Card className="w-[256px] m-4 min-w-[256px]">
+            <Card.Image
+              className="object-fill w-full h-[180px]"
+              src="room.jpg"
+              alt="room"
+            />
+            <Card.Body>
+              <Card.Title tag="h2">Room.pm</Card.Title>
+              <p>Check out the amazing room.pm now!</p>
+              <Card.Actions className="justify-end py-2">
+                <Button color="primary">View Now</Button>
+              </Card.Actions>
+            </Card.Body>
+          </Card>
+
+          <Card className="w-[256px] m-4 min-w-[256px]">
+            <Card.Image
+              className="px-14 py-20 h-[180px]"
+              src="circuit.png"
+              alt="Circuit"
+            />
+            <Card.Body>
+              <Card.Title tag="h2">Circuit!</Card.Title>
+              <p>Check out the amazing circuit.global now!</p>
+              <Card.Actions className="justify-end py-2">
+                <Button color="primary">View Now</Button>
+              </Card.Actions>
+            </Card.Body>
+          </Card>
         </div>
+        {/*  */}
       </div>
     </>
-  )
+  );
 }
-export default Identities
+export default Identities;
