9db348a83a17a509026f7f97228651aacf450cb5
Yofou <exodias1965@gmail.com>
2022-07-28 00:36:45 +0100

    fix/ Hiding sumo ad under email entry & clean up eslint + react migration warnings

--- src/index.js
+++ src/index.js
@@ -5,14 +5,13 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 import ReactDOM from 'react-dom/client'
-const container = document.getElementById('app')
+// const container = document.getElementById('app')
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
