1a4911f523188d20029cbacd8257fbd9b54a92bf
char543 <36009210+char543@users.noreply.github.com>
2022-09-20 09:04:32 +0100

    identities page updates

--- src/components/Identities.jsx
+++ src/components/Identities.jsx
@@ -1,61 +1,50 @@
-import React from "react";
-import { Card, Button } from "react-daisyui";
+import React from "react"; /*  */
+import { Button } from "react-daisyui";
 
 function Identities() {
-  return (
-    <>
-      <div className="flex justify-start items-center h-[100%] pt-24 flex-col overflow-scroll sm:justify-center">
-        <p className="z-20 text-5xl text-white font-bold m-0 p-0 mb-12">
-          Identities
-        </p>
-        <div className="cardContainer flex items-center flex-col flex-wrap mb-16 box-content sm:flex-row sm:justify-center sm:w-full">
-          <Card className="w-[256px] m-4 min-w-[256px]">
-            <Card.Image
-              className="object-fill w-full h-[180px]"
-              src="nightsjourney.jpg"
-              alt="nightsjourney"
-            />
-            <Card.Body>
-              <Card.Title tag="h2">Nights Journey</Card.Title>
-              {/* <p>Check out the amazing Nights Journey now!</p>
-              <Card.Actions className="justify-start py-2 mt-4 -mb-4">
-                <Button color="primary">View Now</Button>
-              </Card.Actions> */}
-            </Card.Body>
-          </Card>
+	return (
+		<>
+			<div className="flex justify-start items-center h-[100%] pt-24 flex-col overflow-scroll sm:justify-center">
+				<p className="z-20 text-5xl text-white font-bold m-0 p-0 mb-12">
+					Identities
+				</p>
 
-          <Card className="w-[256px] m-4 min-w-[256px]">
-            <Card.Image
-              className="px-14 py-20 h-[180px] bg-white bg-opacity-5"
-              src="circuit.png"
-              alt="Circuit"
-            />
-            <Card.Body>
-              <Card.Title tag="h2">Circuit</Card.Title>
-              {/* <p>Check out the amazing circuit.global now!</p>
-              <Card.Actions className="justify-start py-2 mt-4 -mb-4">
-                <Button color="primary">View Now</Button>
-              </Card.Actions> */}
-            </Card.Body>
-          </Card>
+				<div className="wrapper flex flex-col items-center sm:flex-row">
+					<div className="flex flex-col items-center gap-4">
+						<div className="flex flex-col basis-[70%] min-h-[150px] justify-center items-center">
+							<img
+								className="max-w-[200px] basis-[70%] min-w-[200px] min-h-[150px] max-h-[100px] rounded"
+								src="nightsjourney.jpg"
+								alt="nightsjourney"
+							/>
+						</div>
+						<h1 className="flex basis-[30%]">Nights Journey</h1>
+					</div>
 
-          <Card className="w-[256px] m-4 min-w-[256px]">
-            <Card.Image
-              className="object-fill w-full h-[180px]"
-              src="room.jpg"
-              alt="room"
-            />
-            <Card.Body>
-              <Card.Title tag="h2">Room.pm</Card.Title>
-              {/* <p>Check out the amazing room.pm now!</p>
-              <Card.Actions className="justify-start py-2 mt-4 -mb-4">
-                <Button color="primary">View Now</Button>
-              </Card.Actions> */}
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
-    </>
-  );
+					<div className="flex flex-col items-center gap-4">
+						<div className="flex flex-col basis-[70%] min-h-[150px] justify-center items-center bg-gray-500 rounded">
+							<img
+								className="max-w-[200px] min-w-[50px] basis-[70%] max-h-[80px] px-6"
+								src="circuit.png"
+								alt="Circuit"
+							/>
+						</div>
+						<h1 className="flex basis-[30%]">Circuit</h1>
+					</div>
+
+					<div className="flex flex-col items-center gap-4">
+						<div className="flex flex-col basis-[70%] min-h-[150px] justify-center items-center">
+							<img
+								className="max-w-[200px] basis-[70%] min-w-[200px] min-h-[150px] max-h-[100px] rounded bg-white p-4"
+								src="room.jpg"
+								alt="room"
+							/>
+						</div>
+						<h1 className="basis-[30%]">Room</h1>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 export default Identities;
