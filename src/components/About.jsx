836d3a7f55c65573a18e2fd2685f7b30bb70aabe
char543 <36009210+char543@users.noreply.github.com>
2022-09-23 13:50:07 +0100

    updated about us text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function About() {
-  return (
-    <>
-      <div className="flex justify-center items-center h-[100%] flex-col">
-        <h1 className="z-20 text-5xl text-white font-bold m-0 p-0">About Us</h1>
-        <p className="z-20 text-large text-white font-bold m-12 p-0 max-w-2xl text-center">
-          Room Life is an events management company for music both live and
-          virtual events. We use multiple mediums to reach our audiences. We
-          seek new technologies to expand our capabilities and reach, including
-          enhancing the audience experience.
-        </p>
-      </div>
-    </>
-  )
+	return (
+		<>
+			<div className="flex justify-center items-center h-[100%] flex-col">
+				<h1 className="z-20 text-5xl text-white font-bold m-0 p-0">
+					About Us
+				</h1>
+				<p className="z-20 text-large text-white font-bold m-12 p-0 max-w-2xl text-center">
+					Room Life is an events management company for music both
+					live and virtual. We use multiple mediums to reach our
+					audiences and we seek new technologies to expand our
+					capabilities and reach, including enhancing the audience
+					experience.
+				</p>
+			</div>
+		</>
+	);
 }
-export default About
+export default About;
