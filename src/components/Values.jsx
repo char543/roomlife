17642cd30ba1aa1e29b3384966e0271816195f7e
char543 <36009210+char543@users.noreply.github.com>
2022-11-05 05:05:32 +0000

    chnaged texts, removed sumo mail

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 
 function Values() {
-  return (
-    <>
-          <div className="h-screen flex flex-col justify-center items-center z-10">
-            <h1 className="text-5xl font-bold p-10">Values</h1>
-            <p className="py-6 px-8 md:max-w-[920px] text-center">
-              As a collective our drive is to help and support talent on their
-              journey to their peak, within their music and performing careers.
-              That principle is also injected into the whole team within the
-              company to become leaders within their chosen business acumen.
-            </p>
-          </div>
-
-    </>
-  );
+	return (
+		<>
+			<div className="h-screen flex flex-col justify-center items-center z-10">
+				<h1 className="text-5xl font-bold p-10">Values</h1>
+				<p className="py-6 px-8 md:max-w-[920px] text-center">
+					As a collective, our drive is to help and support talent on
+					their journey to their peak, within their music and
+					performing careers. That principle is also injected into the
+					whole team within the company. To become leaders within
+					their chosen business acumen.
+				</p>
+			</div>
+		</>
+	);
 }
 
 // export default Values;
